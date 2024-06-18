918d5babba43a6e93c8687cd218d0e0d5c8cd121
Felipe Ghizoni <felipeghizoni01@gmail.com>
2024-06-17 22:07:06 -0300

    feat: Botão para pesquisar câmera

--- src/main/java/visualizacao/visualizacaoCamera.java
+++ src/main/java/visualizacao/visualizacaoCamera.java
@@ -4,9 +4,11 @@ package visualizacao;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import javax.swing.RowFilter;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 import modelo.Camera;
 
 /**
@@ -14,15 +16,22 @@ import modelo.Camera;
  * @author fe060311
  */
 public class visualizacaoCamera extends javax.swing.JFrame {
-
+    
+    public TableRowSorter sorter;
     public visualizacaoCamera() {
         // Configurações do JFrame
         setTitle("Cameras");
         initComponents();
         this.setLocationRelativeTo(null); // Centraliza o JFrame na tela
-        this.tabelaATT();
+        
+        // Criação do TableRowSorter com o modelo da tabela
+        sorter = new TableRowSorter<>(tabelaCameras.getModel());
+        tabelaCameras.setRowSorter(sorter);
+        
+        // Carrega os dados iniciais da tabela
+        tabelaATT();
     }
-
+    
     Camera camera = new Camera();
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -33,15 +42,18 @@ public class visualizacaoCamera extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        botaoClientes = new javax.swing.JButton();
+        botaoMenu = new javax.swing.JButton();
+        botaoEnvios = new javax.swing.JButton();
         jPaneltabela = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabelaCameras = new javax.swing.JTable();
         addCamera5 = new javax.swing.JButton();
         jButton21 = new javax.swing.JButton();
         botaoExcluir5 = new javax.swing.JButton();
+        textProcurar = new javax.swing.JTextField();
+        botaoProcurar = new javax.swing.JButton();
+        botaoLimpar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -56,16 +68,26 @@ public class visualizacaoCamera extends javax.swing.JFrame {
 
         jPanel3.setBackground(new java.awt.Color(153, 153, 153));
 
-        jButton2.setText("Clientes");
-
-        jButton3.setText("Câmeras");
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        botaoClientes.setText("Clientes");
+        botaoClientes.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                botaoClientesActionPerformed(evt);
             }
         });
 
-        jButton4.setText("Envios");
+        botaoMenu.setText("Menu");
+        botaoMenu.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoMenuActionPerformed(evt);
+            }
+        });
+
+        botaoEnvios.setText("Envios");
+        botaoEnvios.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoEnviosActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
@@ -74,20 +96,20 @@ public class visualizacaoCamera extends javax.swing.JFrame {
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(25, 25, 25)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(botaoEnvios, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(botaoClientes, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(botaoMenu, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(38, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(44, 44, 44)
-                .addComponent(jButton3)
+                .addComponent(botaoMenu)
                 .addGap(18, 18, 18)
-                .addComponent(jButton2)
+                .addComponent(botaoClientes)
                 .addGap(18, 18, 18)
-                .addComponent(jButton4)
+                .addComponent(botaoEnvios)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -109,6 +131,7 @@ public class visualizacaoCamera extends javax.swing.JFrame {
                 return types [columnIndex];
             }
         });
+        tabelaCameras.setShowGrid(false);
         jScrollPane1.setViewportView(tabelaCameras);
         if (tabelaCameras.getColumnModel().getColumnCount() > 0) {
             tabelaCameras.getColumnModel().getColumn(2).setResizable(false);
@@ -156,23 +179,48 @@ public class visualizacaoCamera extends javax.swing.JFrame {
             }
         });
 
+        textProcurar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textProcurarActionPerformed(evt);
+            }
+        });
+
+        botaoProcurar.setText("Procurar");
+        botaoProcurar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoProcurarActionPerformed(evt);
+            }
+        });
+
+        botaoLimpar.setText("Limpar");
+        botaoLimpar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                botaoLimparActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton21, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(addCamera5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(botaoExcluir5, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(41, 41, 41)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(18, 18, 18)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jButton21, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(addCamera5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(botaoExcluir5, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(31, 31, 31)
                         .addComponent(jPaneltabela, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap(16, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(textProcurar, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(botaoProcurar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(botaoLimpar)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel2)
                         .addGap(390, 390, 390))))
@@ -181,19 +229,22 @@ public class visualizacaoCamera extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(18, 18, 18)
-                .addComponent(jLabel2)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(textProcurar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(botaoProcurar)
+                    .addComponent(botaoLimpar))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jPaneltabela, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addContainerGap())
+                    .addComponent(jPaneltabela, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(addCamera5, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(jButton21, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(botaoExcluir5, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(63, 254, Short.MAX_VALUE))))
+                        .addGap(0, 248, Short.MAX_VALUE)))
+                .addContainerGap())
             .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
@@ -230,9 +281,11 @@ public class visualizacaoCamera extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    private void botaoMenuActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoMenuActionPerformed
+        this.setVisible(false);
+        visualizacaoMenu menu = new visualizacaoMenu();
+        menu.setVisible(true);
+    }//GEN-LAST:event_botaoMenuActionPerformed
 
     private void addCamera5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addCamera5ActionPerformed
 
@@ -311,6 +364,37 @@ public class visualizacaoCamera extends javax.swing.JFrame {
         tabelaATT();
     }//GEN-LAST:event_botaoExcluir5ActionPerformed
 
+    private void textProcurarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textProcurarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textProcurarActionPerformed
+
+    private void botaoProcurarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoProcurarActionPerformed
+        String textoPesquisa =  textProcurar.getText().trim();
+        tabelaCameras.setRowSorter(sorter);
+        if (textoPesquisa.length() == 0) {
+            sorter.setRowFilter(null);
+        } else {
+            sorter.setRowFilter(RowFilter.regexFilter(textoPesquisa));
+        }
+    }//GEN-LAST:event_botaoProcurarActionPerformed
+
+    private void botaoLimparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoLimparActionPerformed
+        sorter.setRowFilter(null);
+        tabelaATT();
+    }//GEN-LAST:event_botaoLimparActionPerformed
+
+    private void botaoClientesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoClientesActionPerformed
+        this.setVisible(false);
+        visualizacaoCliente cliente = new visualizacaoCliente();
+        cliente.setVisible(true);
+    }//GEN-LAST:event_botaoClientesActionPerformed
+
+    private void botaoEnviosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoEnviosActionPerformed
+        this.setVisible(false);
+        visualizacaoEnvio envio = new visualizacaoEnvio();
+        envio.setVisible(true);
+    }//GEN-LAST:event_botaoEnviosActionPerformed
+
     public void tabelaATT() {
     DefaultTableModel dtmCameras = (DefaultTableModel) tabelaCameras.getModel();
     ArrayList lista = camera.getCameras();
@@ -375,11 +459,13 @@ public class visualizacaoCamera extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton addCamera5;
+    private javax.swing.JButton botaoClientes;
+    private javax.swing.JButton botaoEnvios;
     private javax.swing.JButton botaoExcluir5;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton botaoLimpar;
+    private javax.swing.JButton botaoMenu;
+    private javax.swing.JButton botaoProcurar;
     private javax.swing.JButton jButton21;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
@@ -388,5 +474,6 @@ public class visualizacaoCamera extends javax.swing.JFrame {
     private javax.swing.JPanel jPaneltabela;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tabelaCameras;
+    private javax.swing.JTextField textProcurar;
     // End of variables declaration//GEN-END:variables
 }
